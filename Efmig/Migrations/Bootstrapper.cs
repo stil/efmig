a6995edbe0b5eb2f70d27c6fb3cd21d023b72e2f
stil <p@ryciuk.com>
2023-03-13 00:37:05 +0100

    Fix log viewer not clearing

--- Efmig/Migrations/Bootstrapper.cs
+++ Efmig/Migrations/Bootstrapper.cs
@@ -202,6 +202,8 @@ public class Bootstrapper
             var action = new CreateMigrationAction();
             await action.ExecuteAsync(context);
         }, profileSelectedAndEnteredMigrationName);
+        
+        mainWindow.LogViewer.Inlines!.Add(new Run("Command result will appear here."));
 
         mainWindow.DataContext = mainWindowViewModel;
         return mainWindow;
